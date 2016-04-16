69940f8df78a30133e02fa64652efb816bfcdbe9
Brody Eller <wviper3@gmail.com>
2016-04-16 12:09:19 -0400

    Initial Commit

---
+++ Demolition Derby/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Demolition Derby
+//
+//  Created by Brody Eller on 4/16/16.
+//  Copyright © 2016 Brody Eller. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
